70b49f1ffc056ee4796463be7bee53a8c5a6f185
Nitish Chaudhary <78808175+Nitiznc@users.noreply.github.com>
2022-12-25 12:40:11 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Services from "./components/Services";
+
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/home.scss";
+import "./styles/mediaquery.scss"
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element = {<Contact />} />
+        <Route path="/services" element = {<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+export default App;
